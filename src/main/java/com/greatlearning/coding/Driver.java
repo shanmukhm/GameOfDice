3faac4447c47ac6480d27afc8cc0325db2095df0
Shanmukh <shanmukh111@gmail.com>
2021-07-02 17:28:58 +0530

    Cleaning

--- src/main/java/com/greatlearning/coding/Driver.java
+++ src/main/java/com/greatlearning/coding/Driver.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter number of players: ");
+
         int N = Integer.parseInt(args[0]);
-//        System.out.println("Enter Target score: ");
         int M = Integer.parseInt(args[1]);
 
         Game game = new Game(N, M);
